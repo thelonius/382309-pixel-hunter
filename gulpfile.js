fda5cdc72baf906665c22e49e4093d1e6c0cc2f1
ed <dubnitsky@gmail.com>
2018-06-06 13:20:17 +0300

    added testing environment

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,21 @@ const rename = require(`gulp-rename`);
 const imagemin = require(`gulp-imagemin`);
 const rollup = require(`gulp-better-rollup`);
 const sourcemaps = require(`gulp-sourcemaps`);
+const mocha = require(`gulp-mocha`);
+const commojs = require(`rollup-plugin-commonjs`);
+
+gulp.task(`test`, function () {
+  return gulp
+  .src([`js/**/*.test.js`])
+  .pipe(rollup({
+    plugins: [
+      commonjs()
+    ]}, `cjs`))
+  .pipe(gulp.dest(`build/test`))
+  .pipe(mocha({
+    reporter: `spec`
+  }));
+});
 
 gulp.task(`style`, () => {
   return gulp.src(`sass/style.scss`).
